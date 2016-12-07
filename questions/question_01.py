8598338ae3a16b9a67e730304ad96b605a9c4440
narendradivi <narendraavula2@gmail.com>
2016-12-07 17:13:06 +0530

    files and controlflow

---
+++ questions/question_01.py
@@ -0,0 +1,17 @@
+__author__ = 'Narendra'
+
+notes = """
+    This is to refresh your bit manipulation skills.
+"""
+
+
+#Convert integer to binary string and return the string.
+#Integers also include negative numbers. So for negative numbers represent in two's complement.
+#Integers are 32-bit integers.
+def integer_to_binary(input):
+    pass
+
+
+#write tests to test your solution
+def test_integer_to_binary():
+    pass
